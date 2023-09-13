dbb20360f201095f8df2213859acc4562488c041
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-09-13 09:32:59 -0400

    a

---
+++ src/use-cases/gym/update-gym.spec.ts
@@ -0,0 +1,38 @@
+import { Gym } from '@prisma/client';
+import { UpdateGymUseCase } from './update-gym';
+import { describe, beforeEach, it, expect } from 'vitest';
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: UpdateGymUseCase;
+
+describe('Update Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new UpdateGymUseCase(gymsRepository);
+  });
+
+  it('should be able to update a gym', async () => {
+    const gym = new Gym({
+      id: '1234567890',
+      title: 'Quebra Dentes',
+      description: 'Somos uma academia de Artes Marciais',
+      phone: '(65)98123-3996',
+      email: 'quebradentes@gmail.com',
+      address_line1: 'aaaaaaaaaaaaaa',
+      address_line2: 'bbbbbbbbbbbbbbbbb',
+      city: 'Várzea Grande',
+      state: 'Mato Grosso',
+      cnpj: '11.111.111/00001-01',
+      country: 'Brasil',
+      password_hash: '123456',
+    });
+
+    const { gym } = await sut.execute({
+      id: gym.id,
+      title: 'Novo Título',
+    });
+
+    expect(gym.title).toEqual('Novo Título');
+  });
+});
