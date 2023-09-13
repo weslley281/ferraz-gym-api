91c951120c5026ef0b9d81389705f44b53423e4d
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-09-13 10:06:55 -0400

    a

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -19,9 +19,7 @@ export class InMemoryGymsRepository implements GymsRepository {
       city: data.city,
       state: data.state,
       country: data.country,
-      created_at: new Date(),
-      updated_at: new Date(),
-    }
+    };
 
     this.items.push(gym);
 
@@ -29,7 +27,25 @@ export class InMemoryGymsRepository implements GymsRepository {
   }
 
   async update(data: Prisma.GymUpdateInput): Promise<Gym> {
-    throw new Error('Method not implemented.');
+    const gym: Gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description,
+      cnpj: data.cnpj,
+      phone: data.phone,
+      email: data.email,
+      password_hash: data.password_hash,
+      address_line1: data.address_line1,
+      address_line2: data.address_line2,
+      city: data.city,
+      state: data.state,
+      country: data.country,
+    };
+
+    const index = 0;
+    this.items.splice(index, 1, gym);
+
+    return gym;
   }
 
   async findById(id: string): Promise<any> {
--- src/use-cases/gym/update-gym.spec.ts
+++ src/use-cases/gym/update-gym.spec.ts
@@ -13,7 +13,7 @@ describe('Update Gym Use Case', () => {
   });
 
   it('should be able to update a gym', async () => {
-    const gym = new Gym({
+    await gymsRepository.create({
       id: '1234567890',
       title: 'Quebra Dentes',
       description: 'Somos uma academia de Artes Marciais',
@@ -29,8 +29,18 @@ describe('Update Gym Use Case', () => {
     });
 
     const { gym } = await sut.execute({
-      id: gym.id,
+      id: '1234567890',
       title: 'Novo Título',
+      description: 'Somos uma academia de Artes Marciais',
+      phone: '(65)98123-3996',
+      email: 'quebradentes@gmail.com',
+      address_line1: 'aaaaaaaaaaaaaa',
+      address_line2: 'bbbbbbbbbbbbbbbbb',
+      city: 'Várzea Grande',
+      state: 'Mato Grosso',
+      cnpj: '11.111.111/00001-01',
+      country: 'Brasil',
+      password_hash: '123456',
     });
 
     expect(gym.title).toEqual('Novo Título');
